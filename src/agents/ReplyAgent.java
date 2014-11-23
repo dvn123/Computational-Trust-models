1fc9f53ecdb9e6844a45508f5673b14578ee376e
dvn123 <diogvaznunes@gmail.com>
2014-11-23 17:15:24 +0000

    Reply Agent

--- src/agents/ReplyAgent.java
+++ src/agents/ReplyAgent.java
@@ -16,6 +16,7 @@ import jade.proto.AchieveREResponder;
 
 import java.util.Random;
 
+import util.Constants;
 import util.Question;
 
 public class ReplyAgent extends Agent {
@@ -24,42 +25,39 @@ public class ReplyAgent extends Agent {
 
     Float[] knowledge;
 
-    ReplyAgent(float additionKnowledge, float subtractionKnowledge, float multiplicationKnowledge, float divisionKnowledge) {
-        knowledge = new Float[] {additionKnowledge, subtractionKnowledge, multiplicationKnowledge, divisionKnowledge};
-    }
-
-    ReplyAgent() {
-        Random rand = new Random();
-        knowledge = new Float[] {rand.nextFloat(), rand.nextFloat(), rand.nextFloat(), rand.nextFloat()};
-    }
-
 
     float reply(Question q) {
         Random rand = new Random();
         float factor = -1;
         factor = knowledge[q.getOperator()];
         return q.getResult()*factor + (1-factor)*(rand.nextInt((MAX - MIN) + 1) + MIN);
-
     }
 
     protected void setup() {
-        System.out.println("Agent "+getLocalName()+" waiting for requests...");
+        Random rand = new Random();
+
+        Object[] args = getArguments();
+        if (args != null && args.length > 0) {
+            knowledge = new Float[] {Float.valueOf((String) args[0]), Float.valueOf((String) args[1]), Float.valueOf((String) args[2]), Float.valueOf((String) args[3])};
+        } else {
+            knowledge = new Float[] {rand.nextFloat(), rand.nextFloat(), rand.nextFloat(), rand.nextFloat()};
+        }
+
 
         try {
             DFAgentDescription dfd = new DFAgentDescription();
             dfd.setName(getAID());
             ServiceDescription sd = new ServiceDescription();
-            sd.setName("replyAgent");
-            sd.setType("replyAgent");
-            // Agents that want to use this service need to "know" the weather-forecast-ontology
-            // Agents that want to use this service need to "speak" the FIPA-SL language
+            sd.setName(Constants.SERVICE_DESCRIPTION_ONTOLOGY_PLAYER);
+            sd.setType(Constants.SERVICE_DESCRIPTION_ONTOLOGY_INFORMATION_SOURCE_FIRE);
             sd.addLanguages(FIPANames.ContentLanguage.FIPA_SL);
             dfd.addServices(sd);
             DFService.register(this, dfd);
-        }
-        catch (FIPAException fe) {
+        } catch (FIPAException fe) {
             fe.printStackTrace();
         }
+
+        System.out.println("Agent "+getLocalName()+" waiting for requests...");
 
         MessageTemplate template = MessageTemplate.and(
                 MessageTemplate.MatchProtocol(FIPANames.InteractionProtocol.FIPA_REQUEST),
--- src/util/Constants.java
+++ src/util/Constants.java
@@ -3,6 +3,9 @@ package util;
 public class Constants {
 	static public String SERVICE_DESCRIPTION_TYPE_PLAYER = "QuemQuerSerMilionario-player";
 	static public String SERVICE_DESCRIPTION_ONTOLOGY_PLAYER = "QuemQuerSerMilionario-player-ontology";
+
+	static public String SERVICE_DESCRIPTION_TYPE_INFORMATION_SOURCE_FIRE = "QuemQuerSerMilionario-information-source-fire";
+	static public String SERVICE_DESCRIPTION_ONTOLOGY_INFORMATION_SOURCE_FIRE = "QuemQuerSerMilionario-information-source-fire-ontology";
 	
 	static public String SERVICE_DESCRIPTION_TYPE_WISE = "QuemQuerSerMilionario-wise";
 	static public String SERVICE_DESCRIPTION_ONTOLOGY_WISE = "QuemQuerSerMilionario-wise-ontology";
