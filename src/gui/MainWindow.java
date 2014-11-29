b1324f5f80d59ef0fc5f543f39afdd6ecd5bf471
Tiago Fernandes <ei11054@fe.up.pt>
2014-11-29 18:52:44 +0000

    working on main window

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -2,7 +2,12 @@ package gui;
 
 import java.awt.EventQueue;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.UIManager;
+import javax.swing.JLabel;
+import javax.swing.JButton;
 
 public class MainWindow {
 
@@ -38,6 +43,35 @@ public class MainWindow {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		DefaultListModel listModel = new DefaultListModel();
+		listModel.addElement("Jane Doe");
+		listModel.addElement("John Smith");
+		listModel.addElement("Kathy Green");
+		
+		JList list = new JList(listModel);
+		list.setBorder(UIManager.getBorder("InternalFrame.border"));
+		
+		list.setBounds(36, 47, 133, 149);
+		frame.getContentPane().add(list);
+		
+		JLabel lblWiseAgents = new JLabel("Wise Agents");
+		lblWiseAgents.setBounds(36, 25, 110, 14);
+		frame.getContentPane().add(lblWiseAgents);
+		
+		JButton btnAdd = new JButton("Add");
+		btnAdd.setBounds(191, 51, 89, 23);
+		frame.getContentPane().add(btnAdd);
+		
+		JButton btnRemove = new JButton("Remove");
+		btnRemove.setBounds(191, 85, 89, 23);
+		frame.getContentPane().add(btnRemove);
+		
+		JButton btnStartSimulation = new JButton("Start simulation");
+		btnStartSimulation.setBounds(282, 227, 142, 23);
+		frame.getContentPane().add(btnStartSimulation);
+		
+		listModel.addElement("Kathy Green");
 	}
-
 }
